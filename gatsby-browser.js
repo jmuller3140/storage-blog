1f2d1535efb55c3d4fb4ec49dafff0b1300b159e
Jimmy Muller <jmuller3140@gmail.com>
2025-01-18 15:15:51 -0500

    asdasd

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,19 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+import posthog from 'posthog-js'
+
+export const onClientEntry = () => {
+  if (typeof window !== 'undefined') {
+    posthog.init(
+      process.env.GATSBY_POSTHOG_API_KEY,
+      {
+        api_host: 'https://app.posthog.com',
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === 'development') posthog.debug()
+        },
+      }
+    )
+  }
+}
