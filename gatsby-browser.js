9982fdd1711173b56225873d1b23cd3ba2b4e21f
Jimmy Muller <jmuller3140@gmail.com>
2025-01-18 16:57:31 -0500

    asdasda

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,16 +12,24 @@ import "prismjs/themes/prism.css"
 import posthog from 'posthog-js'
 
 export const onClientEntry = () => {
+  console.log('onClientEntry fired')  // Debug log
   if (typeof window !== 'undefined' && process.env.GATSBY_POSTHOG_API_KEY) {
+    console.log('PostHog init starting')  // Debug log
     posthog.init(
       process.env.GATSBY_POSTHOG_API_KEY,
       {
         api_host: 'https://app.posthog.com',
         loaded: (posthog) => {
+          console.log('PostHog loaded')  // Debug log
           if (process.env.NODE_ENV === 'development') posthog.debug()
         },
         capture_pageview: true,
       }
     )
+  } else {
+    console.log('PostHog not initialized:', {  // Debug log
+      hasWindow: typeof window !== 'undefined',
+      hasKey: !!process.env.GATSBY_POSTHOG_API_KEY
+    })
   }
 }
